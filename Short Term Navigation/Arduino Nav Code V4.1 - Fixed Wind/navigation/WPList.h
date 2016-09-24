bda6f0a4a15446aa4494ea905cdc1ee3623a8e39
alenamaryh <amh345@cornell.edu>
2016-09-24 14:09:14 -0400

    Create WPList.h

---
+++ Short Term Navigation/Arduino Nav Code V4.1 - Fixed Wind/navigation/WPList.h
@@ -0,0 +1,21 @@
+
+  //Random coordinates for testing
+  coord_t asdf {5,7};
+  coord_t p0 = {0.0,0.0};
+  coord_t p1 = {42.444544, -76.483399}; 
+  coord_t p2 = {42.444821, -76.483566};
+  coord_t p3 = {100,100};
+  
+  //Coordinates in and around the Engineering Quad, Cornell University
+  coord_t olin = {42.445457, -76.484322}; //Olin Hall
+  coord_t hollister = {42.444259, -76.484435}; //Hollister Hall
+  coord_t outsideDuffield = {42.444254, -76.482660}; //Outside West entrance to Duffield Hall, atop the stairs
+  coord_t outsideThurston = {42.444228, -76.483666}; //In front of Thurston Hall
+  coord_t engQuadX = {42.444591, -76.483498}; //Center of Engineering Quad
+  coord_t bottomStairs = {42.444240, -76.483258}; //Bottom of stairs, outside West entrance to Duffield Hall
+  coord_t engQuadRight = {42.4444792, -76.483244}; //Bottom of stair, to the left of North entrance to Duffield Hall
+  coord_t topEngQuad = {42.444866, -76.483531};
+  
+  //Coordinates in and around the Cornell Sailing Center, Cayuga Lake
+  coord_t lakeOut = {42.471004,-76.506175}; //Out in the lake, close to the shore
+  coord_t shore = {42.469033,-76.5040623}; //Far end of the docks
