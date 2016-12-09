d3c2b6e6692be993622809b06bb53dc26d056187
Caleb Koch <cakoch10@users.noreply.github.com>
2016-12-09 00:51:58 -0500

    Update navigation.cpp

--- Arduino Navgation Code/Nav V6/ShortTermNavigation/navigation.cpp
+++ Arduino Navgation Code/Nav V6/ShortTermNavigation/navigation.cpp
@@ -4,6 +4,34 @@
 #include "sensors.h"
 #include "navigation.h"
 
+
+// this class allows us to use a vector data structure within Arduino code
+// Take from https://forum.arduino.cc/index.php?topic=45626.0
+template<typename Data>
+class Vector {
+  size_t d_size; // Stores no. of actually stored objects
+  size_t d_capacity; // Stores allocated capacity
+  Data *d_data; // Stores data
+  public:
+    Vector() : d_size(0), d_capacity(0), d_data(0) {}; // Default constructor
+    Vector(Vector const &other) : d_size(other.d_size), d_capacity(other.d_capacity), d_data(0) { d_data = (Data *)malloc(d_capacity*sizeof(Data));
+        memcpy(d_data, other.d_data, d_size*sizeof(Data)); }; // Copy constuctor
+    ~Vector() { free(d_data); }; // Destructor
+    Vector &operator=(Vector const &other) { free(d_data); d_size = other.d_size; d_capacity = other.d_capacity;
+        d_data = (Data *)malloc(d_capacity*sizeof(Data));
+        memcpy(d_data, other.d_data, d_size*sizeof(Data));
+        return *this; }; // Needed for memory management
+    void push_back(Data const &x) { if (d_capacity == d_size) resize(); d_data[d_size++] = x; }; // Adds new value. If needed, allocates more space
+    size_t size() const { return d_size; }; // Size getter
+    Data const &operator[](size_t idx) const { return d_data[idx]; }; // Const getter
+    Data &operator[](size_t idx) { return d_data[idx]; }; // Changeable getter
+  private:
+    void resize() { d_capacity = d_capacity ? d_capacity*2 : 1; Data *newdata = (Data *)malloc(d_capacity*sizeof(Data)); memcpy(newdata, d_data, d_size * sizeof(Data)); free(d_data); d_data = newdata; };// Allocates double the old space
+};
+
+/*----------PixyCam Variables----------*/
+Vector<double> xVals;
+
 //Define Servo types for Sail and Tail
 Servo tailServo;
 Servo sailServo;
@@ -647,3 +675,22 @@ void nServos(void) {
   sailServo.write(sailAngle);
 }
 
+
+/** Updates xVals vector
+*   Only updates x-position
+*   NOTE: This only detects objects set to
+*   signature 1 on the pixy cam */
+void getObjects() {
+    uint16_t blocks = pixy.getBlocks();
+    if (blocks) {
+        for (int j = 0; j < blocks, j++) {
+            if (pixy.blocks[j].signature == 1) {
+                int32_t xLocation = pixy.blocks[j].x; // range: 0 to 319
+                xVals.push_back(xLocation);
+            }
+        }
+    }
+    else {
+        xVals.push_back(400.0); // no objects were detected
+    }
+}
