133380d39355027d59a8fb390bec644035883182
Alex Pomerenk <alexpomerenk@Alexs-MacBook-Pro.local>
2017-04-09 11:17:27 -0400

    gui changes

--- GUI/gui.py
+++ GUI/gui.py
@@ -10,20 +10,46 @@ def gui(file_name):
   lines= F.readlines()
   waypoints=[]
   gps=[]
-  for x in range(0,len(lines)):
-    if lines[x][:8]=='Latitude':
-      lat=float(lines[x][10:-1])
-      x+=1
-      lon=float(lines[x][10:-1])
-      gps.append([lat,lon])
+  raw_ascii=""
+  # convert log file in hex to a string in ascii
+  for x in range(1,len(lines)):
+    raw=lines[x]
+    index=raw.find("RECV,")+5
+    hex_string=raw[index:-1]
+    ascii_string=hex_string.decode("hex")
+    raw_ascii+=ascii_string
 
+  # split the string by line
+  lines_ascii=raw_ascii.split("\n")
+  # look in the lines for Lat Lon coordinates
+  for x in range(0,len(lines_ascii)):
+    string_data=lines_ascii[x]
+    # print string_data
+    # filter out lines that start w "Latitude"
+    if len(string_data)>8 and string_data[:8]=='Latitude':
+      # get the latitude coordinate and the longitude coord from the next line
+      lat=float(string_data[10:-1])
+      # increment the counter
+      x+=1
+      lon=float(string_data[10:-1])
+      # add to the gps coordinates
+      gps.append([lat,lon])
+    if len(string_data)>13 and string_data[:13]=="Next Waypoint":
+      # get lat and lon of the waypoint from the line
+      index1=string_data.index(',')
+      lat= string_data[18:index1]
+      lon= string_data[index1+2:-1]
+      # if we havent added the waypoint to the waypoints array, add it
+      # probably can be removed, or made more efficient so as not to search
+      if [lat,lon] not in waypoints:
+        waypoints.append([lat,lon])
 
   # bottom stairs, bottom left thurston, middle right of x, bottom small stairs
-  test_waypoints=[[42.444240, -76.483258],[42.444259, -76.484435],[42.444545, -76.483094],[42.444808, -76.483158]]
-  waypoints+=test_waypoints
+  # test_waypoints=[[42.444240, -76.483258],[42.444259, -76.484435],[42.444545, -76.483094],[42.444808, -76.483158]]
+  # waypoints+=test_waypoints
   # testing gps
-  test_gps=[[42.444228, -76.483666],[42.444612, -76.483492],[42.444240, -76.483258],[42.444735, -76.483275],[42.4444792, -76.483244],[42.4448630, -76.4835293]]
-  gps+=test_gps
+  # test_gps=[[42.444228, -76.483666],[42.444612, -76.483492],[42.444240, -76.483258],[42.444735, -76.483275],[42.4444792, -76.483244],[42.4448630, -76.4835293]]
+  # gps+=test_gps
   # rotate 90 degrees ccw then flip across x by x=y, y=x
   wx, wy, gx, gy=[],[],[],[]
   for x in waypoints:
@@ -42,7 +68,7 @@ def gui(file_name):
 if __name__ == "__main__":
   # makes sure gui is run with a file as the target
   if len(sys.argv)!=2:
-    print "RUN WITH python gui.py FILENAME.txt"
+    print "RUN WITH python gui.py FILENAME.log"
   else:
     file_name=sys.argv[1]
     gui(file_name)
