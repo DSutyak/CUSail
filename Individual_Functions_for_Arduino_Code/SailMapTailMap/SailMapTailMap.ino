af6ef0158b0a2c2c8dc9379fbf73cfa9dd1b78a2
cusail <cuautoboat@gmail.com>
2017-04-17 16:37:51 -0400

    Sail map tail map done

--- Individual_Functions_for_Arduino_Code/SailMapTailMap/SailMapTailMap.ino
+++ Individual_Functions_for_Arduino_Code/SailMapTailMap/SailMapTailMap.ino
@@ -22,61 +22,17 @@ void setup() {
 
 void loop() {
 
-/*Sail Basic Test*/
+//  Sail.write(sailMapBench(0)); //Sail to 0
+//  //delay(5000);
+//  
+//  Tail.write(tailMapBench(0, -30));
+//  delay(5000);  
+//  
+//  Tail.write(tailMapBench(0, 0));
+//  delay(5000);
 //
-//  Sail.write(sailMap(0));
-//  Tail.write(tailMap(0,0));
-//  delay(10000);
-//  Tail.write(tailMap(0,0));   
-//  Sail.write(sailMap(90));
+//  Tail.write(tailMapBench(0, 30));
 //  delay(5000);
-//  Sail.write(sailMap(180));
-//  delay(5000);
-//  Sail.write(sailMap(270)); 
-//  delay(5000);
-//  Sail.write(sailMap(360));
-//  delay(5000);
-
-
-/*Sail Complex Test*/
-//  for (double i = 0; i <= 360; i += 20){
-//    Sail.write(sailMap(i));
-//
-//    delay(5000);
-//  }
-
-/*Tail Sweep Raw Servo Test*/
-//  Sail.write(sailMap(0));
-//  for(int i=54; i<=162; i++){
-//    delay(50);
-//  }
-//
-//  for(int i=162; i>=54; i--){
-//    Tail.write(i);
-//    delay(50);
-//  }
-
-  
-/*Tail Non-Sweep Servo Test*/
-//  Tail.write(38); //-30 degrees
-//  delay(5000);
-//  Tail.write(85); //0 degrees
-//  delay(5000);
-//  Tail.write(145); //30 degrees
-//  delay(5000);
-
-  Sail.write(sailMapBench(0)); //Sail to 0
-  //delay(5000);
-  
-
-  Tail.write(tailMapBench(0, -30));
-  delay(5000);  
-  
-  Tail.write(tailMapBench(0, 0));
-  delay(5000);
-
-  Tail.write(tailMapBench(0, 30));
-  delay(5000);
 
 //  Sail.write(sailMapBench(90)); //Sail to 0
 //  delay(5000);
@@ -165,21 +121,7 @@ void loop() {
 //  delay(5000);
 //
 //  Tail.write(tailMap(270, -60));
-//  delay(5000);
-
-
-/*Sail-Tail Complex Test*/
-//  for(int i=60;i<120;i++){
-//    Sail.write(sailMap(i)); 
-//    Tail.write(tailMap(i,90));
-//    delay(70);
-//  }
-//  for(int i=120;i>60;i--){
-//    Sail.write(sailMap(i));
-//    Tail.write(tailMap(i,90));
-//    delay(70);
-//  }
-
+//  delay(5000)
 }
 
 /*-------------------Sail and Tail Mapping Functions---------------------*/
@@ -192,16 +134,16 @@ double sailMap(double sailAngle){
   double newSailAngle;
   
   if (sailAngle <= 90){
-    newSailAngle = map(sailAngle, 0, 90, 71, 91.5);
+    newSailAngle = map(sailAngle, 0, 90, 142, 125.5);
   }
   else if (sailAngle <= 180){
-    newSailAngle = map(sailAngle, 90, 180, 91.5, 110.5);
+    newSailAngle = map(sailAngle, 90, 180, 125.5, 109);
   }
   else if (sailAngle <= 270){
-    newSailAngle = map(sailAngle, 180, 270, 110.5, 128);
+    newSailAngle = map(sailAngle, 180, 270, 109.5, 91);
   }
   else{
-   newSailAngle = map(sailAngle, 270, 360, 128, 146.5);
+   newSailAngle = map(sailAngle, 270, 360,91.5, 74.5);
   }
   return newSailAngle;
 }
@@ -227,11 +169,11 @@ double tailMap(double sailAngle, double tailAngle){
 
   //map to servo commands
   if (newTailAngle <= 0 ){
-    newTailAngle=map(newTailAngle,-30,0,38,84); 
+    newTailAngle=map(newTailAngle,-30,0,130,100); 
   }
   
   else if (newTailAngle > 0 ){
-    newTailAngle=map(newTailAngle,0,30,85,144); 
+    newTailAngle=map(newTailAngle,0,30,100,70); 
   }
 
   return newTailAngle;
