9d2de4b9acc624b3d2d6b41d23eadd6c508c6e61
Alex Merrill <am2254@cornell.edu>
2017-10-28 13:57:18 -0400

    turned origin into external variable

--- Arduino_Navigation_Code/Nav_V16/ShortTermNavigation/navigation_helper.h
+++ Arduino_Navigation_Code/Nav_V16/ShortTermNavigation/navigation_helper.h
@@ -6,6 +6,7 @@
  Provides helper functions for Navigation
 --------------------------------------------------------------------*/
 
+extern coord_xy origin;
 /*Sets the origin of the XY plane to a starting point*/
 void setOrigin(coord_t startPoint)
 
@@ -19,10 +20,10 @@ double xyDist(coord_xy point1, coord_xy point2)
 void avoidObject(void);
 
 /*Returns angle (with respect to North) between two global coordinates.*/
-float angleToTarget(coord_xy point1, coord_xy point2);
+float angleToTarget(float lat1, float long1, float lat2, float long2);
 
 /*Returns great circle distance (in meters) between two global coordinates.*/
-double havDist(coord_xy  coord1, coord_xy coord2);
+double havDist(coord_t  first, coord_t second);
 
 /*converts an angle to a 0-360 range*/
 float convertto360(float angle);
