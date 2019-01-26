f7be5a88538c28a78116cf52d46774dc694ce584
Troy Smith <fencerman2@gmail.com>
2019-01-26 14:24:20 -0500

    added figure structure to _init_

--- new_basestation_py/mpl.py
+++ new_basestation_py/mpl.py
@@ -9,16 +9,15 @@ class mclass:
     def __init__(self,  window):
         self.window = window
         self.window.resizable(0,0)
-        self.plot = Canvas(window, width=600, height=600)
-        self.plot.create_line(0, 600, 600, 0, fill="red", dash=(4, 4))
+        # self.graph = Canvas(window, width=600, height=600)
+        # self.graph.create_line(0, 600, 600, 0, fill="red", dash=(4, 4))
         # self.box = Entry(window)
         self.waypoints= Canvas (window, width=200, height=600, bg="red")
         self.info = Frame (window, bg="white", height=100, width=800)
-        self.plot.grid(row=0, column=0)
+        # self.graph.grid(row=0, column=0)
         self.waypoints.grid(row=0, column=1)
         self.info.grid(row=1, column=0, columnspan=2)
 
-    def plot (self):
         x=np.array ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
         v= np.array ([16,16.31925,17.6394,16.003,17.2861,17.3131,19.1259,18.9694,22.0003,22.81226])
         p= np.array ([16.23697,     17.31653,     17.22094,     17.68631,     17.73641 ,    18.6368,
@@ -28,15 +27,16 @@ class mclass:
         a = fig.add_subplot(111)
         a.scatter(v,x,color='red')
         a.plot(p, range(2 +max(x)),color='blue')
-        a.invert_yaxis()
 
-        a.set_title ("Estimation Grid", fontsize=16)
-        a.set_ylabel("Y", fontsize=14)
-        a.set_xlabel("X", fontsize=14)
+        a.set_title ("Boat Position", fontsize=16)
 
-        canvas = FigureCanvasTkAgg(fig, master=self.window)
-        canvas.get_tk_widget().grid(row=1, column=0, columnspan=2)
-        canvas.draw()
+        self.canvas = FigureCanvasTkAgg(fig, master=self.window)
+        self.canvas.get_tk_widget().grid(row=0, column=0)
+        self.canvas.draw()
+
+
+    def update (self, data):
+        arr=[]
 
 window= Tk()
 start= mclass (window)
--- new_basestation_py/xbeeComm.py
+++ new_basestation_py/xbeeComm.py
@@ -1,13 +1,42 @@
 import serial
 from xbee import XBee
+import re
+import pprint
 
 serial_port = serial.Serial('COM5', 9600)
 xbee = XBee(serial_port)
+pp = pprint.PrettyPrinter(indent=4)
 
+
+# Initialize gui
+
+header = "----------NAVIGATION----------"
+regex = "(?:'rf_data': b')((.|\n)*)'"
+
+data = ""
 while True:
-    try:
-        print (xbee.wait_read_frame())
-    except KeyboardInterrupt:
-        break
+	try:
+		packet = str(xbee.wait_read_frame())
+		# print (packet)
+		match = re.search(regex, packet)
+		if match:
+			line = match.group(1)
+			data += line
+			if (header in data):
+				header_start = data.find(header)
+				header_end = header_start + len(header)
+				data_to_send = data[0:header_start]
+				data_arr = data_to_send.split("\\n")
+				data_assoc = []
+				for datum in data_arr:
+					data_assoc.append(tuple(datum.split(":")))
+				pp.pprint ("Parse data cycle to GUI")
+				# Update gui with data
+				pp.pprint (data_assoc)
+				data = data[header_end:len(data)]
+		else:
+			pp.pprint ("Regex failed to match")
+	except KeyboardInterrupt:
+		break
 
 serial_port.close()
