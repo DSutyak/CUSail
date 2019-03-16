ff21a078743b562fe05af6c2e69c37c1f7dbba15
mahikakudlugi <earth0002@gmail.com>
2019-03-16 14:47:51 -0400

    Added bottom textrow for display info

--- new_basestation_py/pyqtgraphh.py
+++ new_basestation_py/pyqtgraphh.py
@@ -1,13 +1,8 @@
 from PyQt5 import QtGui  # (the example applies equally well to PySide)
 import pyqtgraph as pg
-<<<<<<< HEAD
-pastpoint = (0,0)
-
-=======
 import time as time
 global past_point
 past_point = (0,0)
->>>>>>> cf2be9d42c27c98e5ff4e9d2c080cefaa0832de6
 ## Always start by initializing Qt (only once per application)
 app = QtGui.QApplication([])
 
@@ -16,35 +11,14 @@ w = QtGui.QWidget()
 
 
 ## Create some widgets to be placed inside
-<<<<<<< HEAD
 btn = QtGui.QPushButton('Plot')
 text = QtGui.QLineEdit('Enter Buoy/Waypoint')
-=======
-btn = QtGui.QPushButton('press me')
-text = QtGui.QLineEdit('0,0')
->>>>>>> cf2be9d42c27c98e5ff4e9d2c080cefaa0832de6
 listw = QtGui.QListWidget()
 plot = pg.PlotWidget()
 display1 = QtGui.QLabel('Wind Vector: <x,y,z>')
 display2 = QtGui.QLabel('Tail Vector: <x,y,z>')
 
 
-def clicked():
-    global pastpoint
-    input = text.text()
-    arr = input.split(',')
-    listw.addItem(input)
-    x = float(arr[0])
-    y = float(arr[1])
-    plot.plot([pastpoint[0],x],[pastpoint[1],y])
-    pastpoint = (x,y)
-
-btn.clicked.connect(clicked)
-
-
-plot.plot([1,2,3],[4,5,6])
-#time.sleep(5)
-plot.plot([3,-2],[6,-2])
 
 def clicked():
     global past_point
@@ -56,7 +30,6 @@ def clicked():
     past_point = (x,y)
 
 btn.clicked.connect(clicked)
-
 
 
 ## Create a grid layout to manage the widgets size and position
