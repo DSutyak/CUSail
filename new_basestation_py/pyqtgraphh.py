931a6ffbfc9077cddd31257812109f20ef7d1e1d
Troy Smith <fencerman2@gmail.com>
2019-03-23 14:12:56 -0400

    added roll pitch and yaw data

--- new_basestation_py/pyqtgraphh.py
+++ new_basestation_py/pyqtgraphh.py
@@ -23,48 +23,51 @@ listw = QtGui.QListWidget()
 plot = pg.PlotWidget()
 plot.setLimits(minXRange=500,maxXRange=500,minYRange=500,maxYRange=500)
 display1 = QtGui.QLabel('Wind Angle: <x,y,z>')
-display2 = QtGui.QLabel('Tail Vector: <x,y,z>')
+display2 = QtGui.QLabel('Roll, Pitch, Yaw: <x,y,z>')
 
 
 
 def update():
-    f = open("live_data.txt")
-    raw_data = list(f)[-1]
-    data = json.loads(raw_data)
-    pp.pprint(data)
-    print("\n")
-    x = float(data['X position'][0:-2])
-    y = float(data['Y position'][0:-2])
-    print(x)
-    print("\n")
-    print(y)
-    print("\n")
-    global past_point
-    # listw.addItem(text.text())
-    # arr = text.text().split(',')
-    # x = float(arr[0])
-    # y = float(arr[1])
-    plot.plot([past_point[0], x], [past_point[1], y])
-    past_point = (x,y)
+	f = open("live_data.txt")
+	raw_data = list(f)[-1]
+	data = json.loads(raw_data)
+	pp.pprint(data)
+	print("\n")
+	x = float(data['X position'][0:-2])
+	y = float(data['Y position'][0:-2])
+	# print(x)
+	# print("\n")
+	# print(y)
+	# print("\n")
+	global past_point
+	# listw.addItem(text.text())
+	# arr = text.text().split(',')
+	# x = float(arr[0])
+	# y = float(arr[1])
+	plot.plot([past_point[0], x], [past_point[1], y])
+	past_point = (x,y)
+	display1.setText("Wind Angle: " + data["Wind w.r.t North"][0:-2])
+	display2.setText("Roll, Pitch, Yaw: <"+data["Roll"][0:-2]+","+data["Pitch"][0:-2]+","+data["Boat direction"][0:-2]+" >")
+	
 
 def clicked():
-    global past_point
-    entry = text.text().strip().replace(" ","")
-    try:
-        arr = entry.split(',')
-        if (not isinstance(float(arr[0]),float) or not isinstance(float(arr[1]),float)):
-            raise Exception
-        #if(entry.count(",") > 1):
-            #raise Exception
-        listw.addItem(entry)
-        x = float(arr[0])
-        y = float(arr[1])
+	global past_point
+	entry = text.text().strip().replace(" ","")
+	try:
+		arr = entry.split(',')
+		if (not isinstance(float(arr[0]),float) or not isinstance(float(arr[1]),float)):
+			raise Exception
+		#if(entry.count(",") > 1):
+			#raise Exception
+		listw.addItem(entry)
+		x = float(arr[0])
+		y = float(arr[1])
 
-        plot.plot([x+2, x-2], [y+2, y-2])
-        plot.plot([x+2, x-2], [y-2, y+2])
+		plot.plot([x+2, x-2], [y+2, y-2])
+		plot.plot([x+2, x-2], [y-2, y+2])
 
-    except Exception as e:
-        print("Could not convert string to float: '" + entry + "'")
+	except Exception as e:
+		print("Could not convert string to float: '" + entry + "'")
 
 
 
@@ -75,7 +78,7 @@ layout = QtGui.QGridLayout()
 w.setLayout(layout)
 
 ## Add widgets to the layout in their proper positions
-layout.addWidget(btn, 1, 0)  # button goes in upper-left
+layout.addWidget(btn2, 1, 0)  # button goes in upper-left
 layout.addWidget(text, 0, 0)  # text edit goes in middle-left
 layout.addWidget(listw, 2, 0)  # list widget goes in bottom-left
 layout.addWidget(display1, 3, 0)  # display1 widget goes in bottom-left
