83b9887a302e5dfc980b39db5451ff9a7ae10732
MTEvrest <eadamwhite@gmail.com>
2019-04-20 11:31:43 -0400

    added the test suite

---
+++ Arduino_Navigation_Code/Nav_V17/testSuite/print.h
@@ -0,0 +1,15 @@
+/*-----------------------------------------------------------------
+ CU Sail
+ Cornell University Autonomous Sailboat Team
+
+ Print
+ Provides print functions for Navigation
+--------------------------------------------------------------------*/
+
+void printData();
+
+void printWaypointData();
+
+void printHitWaypointData();
+
+void printSailTailSet();
